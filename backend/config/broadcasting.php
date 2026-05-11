f319b51163537636e44d405093a309214245a635
MOHAMED <mohamedkab2006@gmail.com>
2026-05-11 21:14:26 +0100

    feat: add broadcasting configuration file for Reverb, Pusher, and Ably support

--- backend/config/broadcasting.php
+++ backend/config/broadcasting.php
@@ -42,7 +42,8 @@ return [
                 'useTLS' => env('REVERB_SCHEME', 'https') === 'https',
             ],
             'client_options' => [
-                // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html
+                'timeout' => 3,
+                'connect_timeout' => 3,
             ],
         ],
 
@@ -60,7 +61,8 @@ return [
                 'useTLS' => env('PUSHER_SCHEME', 'https') === 'https',
             ],
             'client_options' => [
-                // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html
+                'timeout' => 3,
+                'connect_timeout' => 3,
             ],
         ],
 
